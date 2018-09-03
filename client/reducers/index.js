782c414fddab3104d3154257e287f512f116d727
Ionut Cirja <ionut.cirja@gmail.com>
2018-09-03 12:20:36 +0100

    main entry for reducers / plug in search reducer and redux form reducer

---
+++ client/reducers/index.js
@@ -0,0 +1,9 @@
+// @flow
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import search from './search';
+
+export default combineReducers({
+  form: formReducer,
+  search,
+});
