6370148800428bf4417b0264da1915b16d70be7e
Ionut Cirja <ionut.cirja@gmail.com>
2018-09-03 12:25:12 +0100

    create app main entry file / init api with base url / create store / create main theme / render

---
+++ client/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import api from 'superagent-wrapper';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import createStore from './store';
+import App from './components/app';
+import rootSaga from './sagas';
+
+api.init('https://api.punkapi.com/v2/', {});
+
+const initialData = {};
+const store = createStore(initialData);
+store.runSaga(rootSaga);
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#DC143C',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#ffffff',
+      contrastText: '#DC143C',
+    },
+  },
+});
+
+render(
+  <Provider store={store}>
+    <CssBaseline>
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
+    </CssBaseline>
+  </Provider>,
+  document.getElementById('root'),
+);
