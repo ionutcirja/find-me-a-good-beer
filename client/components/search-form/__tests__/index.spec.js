e993283cdbd02cd27392f1dfffceaecb763438c7
Ionut Cirja <ionut.cirja@gmail.com>
2018-09-03 17:29:29 +0100

    search form style update / show overall error message

--- client/components/search-form/__tests__/index.spec.js
+++ client/components/search-form/__tests__/index.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Button } from '@material-ui/core';
+import { Button, FormHelperText } from '@material-ui/core';
 import Component from '..';
 
 describe('SearchForm component', () => {
@@ -16,33 +16,39 @@ describe('SearchForm component', () => {
   
   describe('render', () => {
     it('should render a form with a submit method', () => {
-      const wrapper = shallow(<Component {...propsToRender} />);
+      const wrapper = shallow(<Component {...propsToRender} />).shallow();
       expect(wrapper.find('form').props().onSubmit).toEqual(propsToRender.handleSubmit);
     });
     
     it('should render a submit button which should be disabled if form is pristine or submitting', () => {
-      let wrapper = shallow(<Component {...propsToRender} />);
+      let wrapper = shallow(<Component {...propsToRender} />).shallow();
       let buttonProps = wrapper.find(Button).props();
       expect(buttonProps.type).toEqual('submit');
       expect(buttonProps.disabled).toEqual(false);
       
       propsToRender.submitting = true;
-      wrapper = shallow(<Component {...propsToRender} />);
+      wrapper = shallow(<Component {...propsToRender} />).shallow();
       buttonProps = wrapper.find(Button).props();
       expect(buttonProps.disabled).toEqual(true);
   
       propsToRender.submitting = false;
       propsToRender.pristine = true;
-      wrapper = shallow(<Component {...propsToRender} />);
+      wrapper = shallow(<Component {...propsToRender} />).shallow();
       buttonProps = wrapper.find(Button).props();
       expect(buttonProps.disabled).toEqual(true);
     });
     
     it('should render a food text FormField component', () => {
-      const wrapper = shallow(<Component {...propsToRender} />);
+      const wrapper = shallow(<Component {...propsToRender} />).shallow();
       const tagProps = wrapper.find('FormField').props();
       expect(tagProps.type).toEqual('text');
       expect(tagProps.name).toEqual('food');
+    });
+  
+    it('should render an error message if error prop value is defined', () => {
+      propsToRender.error = 'Some overall error message';
+      const wrapper = shallow(<Component {...propsToRender} />).shallow();
+      expect(wrapper.find(FormHelperText).childAt(0).text()).toEqual(propsToRender.error);
     });
   });
 });
--- client/components/search-form/index.js
+++ client/components/search-form/index.js
@@ -1,10 +1,24 @@
 // @flow
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import type { FormProps } from 'redux-form';
-import { Grid, Button } from '@material-ui/core';
+import { Grid, Button, FormHelperText } from '@material-ui/core';
 import FormField from '../form-field';
 
-const SearchForm = ({ handleSubmit, pristine, submitting }: FormProps) => (
+const styles = {
+  button: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+};
+
+const SearchForm = ({
+  handleSubmit,
+  pristine,
+  submitting,
+  error,
+  classes: { button },
+}: FormProps) => (
   <form onSubmit={handleSubmit}>
     <Grid
       container
@@ -17,14 +31,16 @@ const SearchForm = ({ handleSubmit, pristine, submitting }: FormProps) => (
         label="Good evening Jacek! What would you like to have for dinner tonight?"
       />
       <Button
+        className={button}
         type="submit"
         variant="contained"
         color="primary"
         disabled={pristine || submitting}
       >
-        Find me a fancy beer
+        {submitting ? 'Searching' : 'Find me a fancy beer'}
       </Button>
+      {error && <FormHelperText>{error}</FormHelperText>}
     </Grid>
   </form>);
 
-export default SearchForm;
+export default withStyles(styles)(SearchForm);
