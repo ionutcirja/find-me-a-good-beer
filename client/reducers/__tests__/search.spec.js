c153f6a71cc1875a1653773583240f2bc7873df3
Ionut Cirja <ionut.cirja@gmail.com>
2018-09-03 13:17:14 +0100

    add updateBeerList method to search reducer

---
+++ client/reducers/__tests__/search.spec.js
@@ -0,0 +1,17 @@
+import deepFreeze from 'deep-freeze';
+import reducer from '../search';
+
+describe('Search reducer', () => {
+  it('should return the default state if the action type is not defined', () => {
+    const initialState = [{ name: 'Paulaner' }];
+    deepFreeze(initialState);
+    expect(reducer(initialState, { type: 'UNDEFINED_ACTION' })).toEqual(initialState);
+  });
+  
+  it('should return a new state containing the payload data if action type is UPDATE_BEER_LIST ', () => {
+    const initialState = [];
+    const payload = [{ name: 'Paulaner' }];
+    deepFreeze(initialState);
+    expect(reducer(initialState, { type: 'UPDATE_BEER_LIST', payload })).toEqual(payload);
+  });
+});
--- client/reducers/search.js
+++ client/reducers/search.js
@@ -1,8 +1,8 @@
 // @flow
 import { handleActions } from 'redux-actions';
-import { BeerList } from '../constants/types';
+import { BeerList, Action } from '../constants/types';
 import * as SearchActions from '../actions/search';
 
 export default handleActions({
-  [SearchActions.searchForBeer]: (state: BeerList) => state,
+  [SearchActions.updateBeerList]: (state: BeerList, action: Action) => action.payload,
 }, []);
