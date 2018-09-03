1a6bd15753c5ece7852795ce080b64aaf7d13a76
Ionut Cirja <ionut.cirja@gmail.com>
2018-09-03 16:33:48 +0100

    refactoring

--- client/reducers/search.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import { handleActions } from 'redux-actions';
-import { BeerList, Action } from '../constants/types';
-import * as SearchActions from '../actions/search';
-
-export default handleActions({
-  [SearchActions.updateBeerList]: (state: BeerList, action: Action) => action.payload,
-}, []);
